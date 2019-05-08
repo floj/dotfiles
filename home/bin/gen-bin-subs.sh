094e7791bd3dc555290d9eb61a37a30b601ec7d8
floj <flo.juettner@gmail.com>
2019-05-08 10:48:43 +0200

    added script to generate bin stubs

---
+++ home/bin/gen-bin-subs.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -euo pipefail
+# remove $HOME/bin from path there. We want to find the real binaries
+PATH=":$PATH:"
+PATH="${PATH//:$HOME\/bin:/:}"
+PATH="${PATH#:}"
+PATH="${PATH%:}"
+
+if command -v aws-vault &>/dev/null; then
+  for f in aws packer terraform; do
+    targetBin=$(command -v "$f")
+    echo "Generating binstub for $f ($targetBin)"
+
+    cat<<EOF>"$HOME/bin/$f"
+#!/usr/bin/env bash
+if [[ -z \$AWS_VAULT_PROFILE ]]; then
+  echo "AWS_VAULT_PROFILE not set"
+  exit 1
+fi
+exec aws-vault --prompt=automfa exec "\$AWS_VAULT_PROFILE" -- "$targetBin" "\$@"
+EOF
+  chmod +x "$HOME/bin/$f"
+  done
+fi
+
