8efd303bd34ba17829da6f0699b435ef764ce2e3
floj <flo.juettner@gmail.com>
2019-05-20 08:42:19 +0200

    use .aws-profile instead of .aws-vault-profile

--- home/bin/gen-bin-subs.sh
+++ home/bin/gen-bin-subs.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -uo pipefail
 # remove $HOME/bin from path there. We want to find the real binaries
 PATH=":$PATH:"
 PATH="${PATH//:$HOME\/bin:/:}"
 PATH="${PATH#:}"
 PATH="${PATH%:}"
 
-if command -v aws-vault &>/dev/null; then
-  for f in aws packer terraform; do
+# if command -v aws-vault &>/dev/null; then
+  for f in aws packer terraform cw; do
     targetBin=$(command -v "$f")
+    if [[ -z $targetBin ]]; then
+      echo "$f not found in path, skipping"
+      continue
+    fi
     echo "Generating binstub for $f ($targetBin)"
 
     cat<<EOF>"$HOME/bin/$f"
@@ -17,13 +21,15 @@ set -e
 if [[ -n \$AWS_VAULT ]]; then
   exec "$targetBin" "\$@"
 fi
-if [[ -z \$AWS_VAULT_PROFILE ]]; then
-  echo "AWS_VAULT_PROFILE not set"
-  exit 1
+aws_profile=\${AWS_PROFILE}
+if [[ -z \$aws_profile ]]; then
+  exec "$targetBin" "\$@"
 fi
-exec aws-vault --prompt=automfa exec "\$AWS_VAULT_PROFILE" -- "$targetBin" "\$@"
+exec aws-vault --prompt=automfa exec "\$aws_profile" -- "$targetBin" "\$@"
 EOF
   chmod +x "$HOME/bin/$f"
   done
-fi
+# else
+#   echo "aws-vault not found"
+# fi
 
