4efecdc343a9f58f6a1812e71edc9e30ac9c6f1c
floj <flo.juettner@gmail.com>
2020-08-27 22:33:03 +0200

    update athens proxy script

--- home/bin/run-gomod-proxy.sh
+++ home/bin/run-gomod-proxy.sh
@@ -6,13 +6,12 @@ if docker inspect athens-proxy &>/dev/null; then
   docker rm athens-proxy
 fi
 
-export ATHENS_STORAGE=$HOME/.cache/athens/storage
-[[ -d $ATHENS_STORAGE ]] || mkdir -p "$ATHENS_STORAGE"
-docker run -d -v "$ATHENS_STORAGE:/var/lib/athens" \
+docker run -d -v "athens:/var/lib/athens" \
    -e ATHENS_DISK_STORAGE_ROOT=/var/lib/athens \
+   -e ATHENS_GO_BINARY_ENV_VARS='GOPROXY=proxy.golang.org,direct' \
    -e ATHENS_STORAGE_TYPE=disk \
    --name athens-proxy \
+   -p 9009:3000 \
    --restart always \
-   -p 3330:3000 \
    gomods/athens:latest
  docker logs -f athens-proxy
