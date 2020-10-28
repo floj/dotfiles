2a58dfc736073db9cab593fdba75a000c7a3c160
floj <flo.juettner@gmail.com>
2020-10-28 14:26:03 +0100

    move setting of path

--- zsh/includes/exports.zsh
+++ zsh/includes/exports.zsh
@@ -6,6 +6,3 @@ export SYSTEMD_EDITOR=$EDITOR
 
 export GREP_COLOR='37;45'
 
-# include users local bin dir in path first
-[[ -d $HOME/.local/bin ]] && path=($HOME/.local/bin $path)
-[[ -d $HOME/bin ]] && path=($HOME/bin $path)
