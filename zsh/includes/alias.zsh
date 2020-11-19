d3bba104b7c82c53408f660402601bc0fcf6e397
floj <flo.juettner@gmail.com>
2020-11-19 11:13:33 +0100

    remove unused alias

--- zsh/includes/alias.zsh
+++ zsh/includes/alias.zsh
@@ -95,6 +95,4 @@ alias d-c='docker-compose'
 #
 alias update-mirrors='sudo reflector --verbose -c Germany -p https --age 12 --sort score -f 5 --save /etc/pacman.d/mirrorlist'
 
-alias youtube-dl-mp3="youtube-dl --extract-audio --audio-format mp3"
-
 alias whatismyip='curl ifconfig.me'
