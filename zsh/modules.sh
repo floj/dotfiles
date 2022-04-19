2182a36b0fc26d180d470b2d114cb10cb1970ce9
floj <floj@users.noreply.github.com>
2022-04-19 12:56:37 +0200

    fix subtrees to consider branches

--- zsh/modules.sh
+++ zsh/modules.sh
@@ -7,6 +7,11 @@ subtrees["zsh-history-substring-search"]="zsh-users/zsh-history-substring-search
 subtrees["fast-syntax-highlighting"]="zdharma/fast-syntax-highlighting"
 export subtrees
 
+typeset -A subtreeBranches
+subtrees["zsh-history-substring-search"]="master"
+subtrees["fast-syntax-highlighting"]="main"
+export subtreeBranches
+
 typeset -a moduleOrder
 #moduleOrder=(zsh-completions z zsh-history-substring-search fast-syntax-highlighting)
 moduleOrder=(zoxide zsh-history-substring-search fast-syntax-highlighting)
