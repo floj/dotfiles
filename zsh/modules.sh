67d53dcfbddf4380da4283ae81a579e13436137a
floj <floj@users.noreply.github.com>
2022-05-18 15:19:39 +0200

    add forgit

--- zsh/modules.sh
+++ zsh/modules.sh
@@ -5,14 +5,16 @@
 typeset -A subtrees
 subtrees["zsh-history-substring-search"]="zsh-users/zsh-history-substring-search"
 subtrees["fast-syntax-highlighting"]="zdharma/fast-syntax-highlighting"
+subtrees["forgit"]="wfxr/forgit"
 export subtrees
 
 typeset -A subtreeBranches
 subtreeBranches["zsh-history-substring-search"]="master"
 subtreeBranches["fast-syntax-highlighting"]="main"
+subtreeBranches["forgit"]="master"
 export subtreeBranches
 
 typeset -a moduleOrder
 #moduleOrder=(zsh-completions z zsh-history-substring-search fast-syntax-highlighting)
-moduleOrder=(zoxide zsh-history-substring-search fast-syntax-highlighting)
+moduleOrder=(zoxide zsh-history-substring-search fast-syntax-highlighting forgit)
 export moduleOrder
