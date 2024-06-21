6c35339145e672839cd35e6ddbbc76cad7ca85cb
floj <floj@users.noreply.github.com>
2024-06-21 08:21:23 +0200

    nvim config: remove terraform-ls cmp

--- config/nvim/lua/user/cmp.lua
+++ config/nvim/lua/user/cmp.lua
@@ -19,7 +19,7 @@ cmp.setup({
 	},
 	sources = {
 		{ name = "path" },
-		{ name = "nvim_lsp", keyword_length = 3 },
+		{ name = "nvim_lsp", keyword_length = 1 },
 		{ name = "buffer", keyword_length = 3 },
 		{ name = "luasnip", keyword_length = 2 },
 	},
